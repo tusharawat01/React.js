9148b88d6216f14334d239c09c307a8747c8622a
tusharawat01 <tushar0402tr@gmail.com>
2024-05-30 20:06:48 +0530

    Register form

--- src/component/register form/Register.jsx
+++ src/component/register form/Register.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register() {
+
+  const [error, setError] = useState("")
+  const [submitted, setSubmitted] =useState(false)
+  const [data, setData] = useState([])
 
   const handleForm = (e) => {
     e.preventDefault();
     let form = e.target;
     let formData = new FormData(form);
     let formObj = Object.fromEntries(formData.entries());
-    console.log("Output : ", formObj);
+
+    setError("")
+    if(formObj.password !== formObj.confirmPassword){
+      setError("Password does not match")
+      return;
+    }
+
+    setSubmitted(true);
+    form.reset();
+
+    const newData = {
+      id: data.length + 1,
+      ...formObj
+    }
+
+    setData([...data,newData])
+
+    setTimeout(() => {
+      setSubmitted(false)
+    },3000)
+
+    console.log(JSON.stringify(formObj))
   }
+
+  
+  const removeData = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
 
   return (
     <>
-      <form onSubmit={handleForm} className='flex flex-col z-0 w-1/2 h-3/4 bg-gray-300 p-4'>
+      <div className='flex'>
+      <form onSubmit={handleForm} className='flex flex-col z-0 w-5/12 h-fit bg-gray-300 p-4'>
         <h1 className='text-5xl font-bold text-gray-800 mb-10'>Register Form</h1>
+        {error && <h3 className=' text-orange-500'>{error}</h3>}
+        {submitted && <h3 className='text-green-500'>Form submitted successfully!</h3>}
         <div className='mb-4 flex flex-col items-start'>
           <label htmlFor="firstName" className="text-lg">First Name: </label>
           <input
@@ -98,6 +131,19 @@ function Register() {
           <button type="submit" className='px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500'>Submit</button>
         </div>
       </form>
+      <div className='ml-10 text-center'>
+        <h2 className='text-3xl font-bold text-gray-800 mb-4'>Submissions</h2>
+        <ul className='pl-5'>
+          {data.map((val) => (
+            <li key={val.id} className='mb-2 flex justify-between items-center'>
+              <span>{val.id}. {val.firstName} {val.lastName} - {val.email}, {val.gender}</span>
+              <button className='ml-4 px-2 py-1 bg-red-500 text-white rounded-md shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500' onClick={() => removeData(val.id)}>REMOVE</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      </div>
+     
     </>
   );
 }
